e22c89bd592261db3d51e39e3ffbd330a86a9318
Vladimir Tucek <vladimir.tucek@gmail.com>
2014-04-09 15:11:32 -0400

    Update storage_provider.rb

--- lib/carrierwave-activerecord/storage/storage_provider.rb
+++ lib/carrierwave-activerecord/storage/storage_provider.rb
@@ -32,7 +32,7 @@ module CarrierWave
         # mounted column.  The identifier should be nil if there is no filename.
         def identifier
           if uploader.filename
-            @identifier ||= Digest::SHA1.hexdigest( "#{Time.now.to_i + rand(1000)}" )
+            @identifier ||= "#{Digest::MD5.hexdigest(uploader.file.read)}.#{uploader.file.extension}"
           end
         end
 
