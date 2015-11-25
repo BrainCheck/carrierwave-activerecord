2f508468eb5a5442f7aaed0bb68bfb38dbceaa25
Tuckie <Tuckie@users.noreply.github.com>
2015-11-25 14:10:30 -0500

    Removed id from migration. It is assumed.

--- lib/generators/templates/create_carrierwave_files.rb
+++ lib/generators/templates/create_carrierwave_files.rb
@@ -1,7 +1,6 @@
 class CreateCarrierwaveFiles < ActiveRecord::Migration
   def change
     create_table :carrier_wave_files do |t|
-      t.string :id
       t.string :medium_hash
       t.binary :binary
 
